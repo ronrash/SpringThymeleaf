d78a40421a0d65df09efcc025bbd7928ab223bf3
Rohit Prashar <rohit.prashar@hmcts.net>
2022-09-09 11:25:08 +0100

    Thmeleaf intro

---
+++ src/main/java/thymeleaf/thymeleafdemonew/ThymeleafdemonewApplication.java
@@ -0,0 +1,13 @@
+package thymeleaf.thymeleafdemonew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafdemonewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafdemonewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/thymeleaf/thymeleafdemonew/ThymeleafdemonewApplicationTests.java
@@ -0,0 +1,13 @@
+package thymeleaf.thymeleafdemonew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafdemonewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
